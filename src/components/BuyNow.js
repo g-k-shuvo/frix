55452bf1a551ee304de64724ded0c17d3c5b6b74
gkshuvo <gkshuvo24@gmail.com>
2023-01-27 21:49:34 +0600

    Used Gatsby Image to Buy Now Section

--- src/components/BuyNow.js
+++ src/components/BuyNow.js
@@ -1,20 +1,23 @@
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
 import Checkout from "../components/Checkout"
 
 const BuyNow = (bookData) => {
+  const bookImage = getImage(bookData.bookData.bookImage)
   return (
     <section id="buy-now" className="section-padding">
       <Container>
         <Row className="gy-5">
           <Col lg={7} md={6}>
             <div className="buy-image">
-              <StaticImage
-                src="../images/book.png"
-                alt="Frix"
-                className="navbar-logo"
-              />
+            <GatsbyImage
+                  image={bookImage}
+                  alt={bookData.bookData.description}
+                />
+
             </div>
           </Col>
           <Col lg={{ span: 4, offset: 1 }} md={6}>
